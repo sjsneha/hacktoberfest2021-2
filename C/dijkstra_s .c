32c20d8dde44a324b931c253b52b598fa065479a
SNEHA JAYSHRI <77097698+sjsneha@users.noreply.github.com>
2021-10-02 19:41:43 +0530

    update
    update code

--- C/dijkstra_s .c
+++ C/dijkstra_s .c
@@ -1,120 +1,95 @@
-// Dijkstra's Algorithm in C
+#include<stdio.h>
+#include<limits.h>
+#define v 8
 
-#include <stdio.h>
-#define INFINITY 9999
-#define MAX 10
+struct dijkstra
+{
+    int dist;
+    int p;
+    int visited;
+}graph[v];
 
-void Dijkstra(int Graph[MAX][MAX], int n, int start);
+void initialize(int s){
+    for(int i=0;i<v;i++)
+    {
+        graph[i].dist=INT_MAX;
+        graph[i].p=-1;
+        graph[i].visited=0;
+    }
+    graph[s].dist=0;
+}
 
-void Dijkstra(int Graph[MAX][MAX], int n, int start) {
-  int cost[MAX][MAX], distance[MAX], pred[MAX];
-  int visited[MAX], count, mindistance, nextnode, i, j;
-
-  // Creating cost matrix
-  for (i = 0; i < n; i++)
-    for (j = 0; j < n; j++)
-      if (Graph[i][j] == 0)
-        cost[i][j] = INFINITY;
-      else
-        cost[i][j] = Graph[i][j];
-
-  for (i = 0; i < n; i++) {
-    distance[i] = cost[start][i];
-    pred[i] = start;
-    visited[i] = 0;
-  }
-
-  distance[start] = 0;
-  visited[start] = 1;
-  count = 1;
-
-  while (count < n - 1) {
-    mindistance = INFINITY;
-
-    for (i = 0; i < n; i++)
-      if (distance[i] < mindistance && !visited[i]) {
-        mindistance = distance[i];
-        nextnode = i;
-      }
-
-    visited[nextnode] = 1;
-    for (i = 0; i < n; i++)
-      if (!visited[i])
-        if (mindistance + cost[nextnode][i] < distance[i]) {
-          distance[i] = mindistance + cost[nextnode][i];
-          pred[i] = nextnode;
+int findMin(){
+    int min=INT_MAX;
+    int k=-1;
+    for(int i=0;i<v;i++)
+    {
+        if(graph[i].visited==0)
+        {
+            if(min>graph[i].dist)
+            {
+                min=graph[i].dist;
+                k=i;
+            }
         }
-    count++;
-  }
+    }
+    return k;
+}
 
-  // Printing the distance
-  for (i = 0; i < n; i++)
-    if (i != start) {
-      printf("\nDistance from source to %d: %d", i, distance[i]);
+void findPath(int s,int d){
+    if(graph[d].p==s)
+    {
+        printf("%d->%d->",s,d);
+        return;
+    }
+    else
+    {
+        findPath(s,graph[d].p);
+        printf("%d->",d);
     }
 }
-int main() {
-  int Graph[MAX][MAX], i, j, n, u;
-  n = 7;
 
-  Graph[0][0] = 0;
-  Graph[0][1] = 0;
-  Graph[0][2] = 1;
-  Graph[0][3] = 2;
-  Graph[0][4] = 0;
-  Graph[0][5] = 0;
-  Graph[0][6] = 0;
+void relax(int x,int y,int w){
+    if(graph[y].dist > (graph[x].dist+w))
+    {
+        graph[y].dist = graph[x].dist+w;
+        graph[y].p=x;
+    }
+}
 
-  Graph[1][0] = 0;
-  Graph[1][1] = 0;
-  Graph[1][2] = 2;
-  Graph[1][3] = 0;
-  Graph[1][4] = 0;
-  Graph[1][5] = 3;
-  Graph[1][6] = 0;
+void dijkstraFun(int G[v][v],int source){
+    initialize(source);
+    for(int i=0;i<v;i++)
+    {
+        int k=findMin();
+        for(int j=0;j<v;j++)
+        {
+            if(G[k][j]!=0)
+            {
+                relax(k,j,G[k][j]);
+            }
+        }
+        graph[k].visited=1;
+    }
+}
 
-  Graph[2][0] = 1;
-  Graph[2][1] = 2;
-  Graph[2][2] = 0;
-  Graph[2][3] = 1;
-  Graph[2][4] = 3;
-  Graph[2][5] = 0;
-  Graph[2][6] = 0;
-
-  Graph[3][0] = 2;
-  Graph[3][1] = 0;
-  Graph[3][2] = 1;
-  Graph[3][3] = 0;
-  Graph[3][4] = 0;
-  Graph[3][5] = 0;
-  Graph[3][6] = 1;
-
-  Graph[4][0] = 0;
-  Graph[4][1] = 0;
-  Graph[4][2] = 3;
-  Graph[4][3] = 0;
-  Graph[4][4] = 0;
-  Graph[4][5] = 2;
-  Graph[4][6] = 0;
-
-  Graph[5][0] = 0;
-  Graph[5][1] = 3;
-  Graph[5][2] = 0;
-  Graph[5][3] = 0;
-  Graph[5][4] = 2;
-  Graph[5][5] = 0;
-  Graph[5][6] = 1;
-
-  Graph[6][0] = 0;
-  Graph[6][1] = 0;
-  Graph[6][2] = 0;
-  Graph[6][3] = 1;
-  Graph[6][4] = 0;
-  Graph[6][5] = 1;
-  Graph[6][6] = 0;
-
-  u = 0;
-  Dijkstra(Graph, n, u);
-
-  return 0;
+int main(){
+    int G[v][v]={{0,0,4,0,0,7,0,0},
+                 {0,0,0,0,9,0,0,3},
+                 {4,0,0,3,0,2,9,0},
+                 {0,0,3,0,3,0,7,0},
+                 {0,9,0,3,0,0,2,7},
+                 {7,0,2,0,0,0,8,0},
+                 {0,0,9,7,2,8,0,3},
+                 {0,3,0,0,7,0,3,0}};
+    dijkstraFun(G,0);
+    printf("\nThe updated weights for each node are :");
+    printf("\nNode\tdist\tparent\tvisited");
+    for(int i=0;i<v;i++)
+    {
+        printf("\n%d-> \t%d \t%d \t%d",i,graph[i].dist,graph[i].p,graph[i].visited);
+    }
+    printf("\n");
+    findPath(0,1);
+    return 0;
 }
